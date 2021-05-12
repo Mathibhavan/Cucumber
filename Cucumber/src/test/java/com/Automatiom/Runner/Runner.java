8f4088d70fdbb42fe23ae396ea5e20105915b0b8
Mathi <mlatha2896@gmail.com>
2021-05-12 18:00:40 +0530

    Cucumber commit

---
+++ Cucumber/src/test/java/com/Automatiom/Runner/Runner.java
@@ -0,0 +1,30 @@
+package com.Automatiom.Runner;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import com.cucumber.Cucumber.Base_Class;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(features="C:\\Users\\721901\\eclipse-workspace\\Cucumber\\src\\test\\java\\com\\Automatiom\\Feature\\Cucumber.Feature",
+glue="com\\Automation\\Stepdefinition", monochrome= true, dryRun=false,strict=true)
+
+public class Runner {
+
+	public static WebDriver driver;
+	
+@BeforeClass
+public static void Setup() {
+	driver=Base_Class.BrowserLaunch("Chrome");
+}
+@AfterClass
+public static void Teardown() {
+	driver.quit();
+}
+}
---
+++ Cucumber/src/test/java/com/Automation/Stepdefinition/Login_Stepdefinition.java
@@ -0,0 +1,70 @@
+package com.Automation.Stepdefinition;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import com.Automatiom.Runner.Runner;
+import com.cucumber.Cucumber.Base_Class;
+
+import cucumber.api.java.en.*;
+
+public class Login_Stepdefinition extends Base_Class{
+
+public static WebDriver driver= Runner.driver;
+		
+		@Given("^Launch the URL for hotel booking$")
+		public void launch_the_URL_for_hotel_booking() throws Throwable {
+			getURL("http://adactinhotelapp.com/index.php");
+		}
+
+		@When("^User enter \"([^\"]*)\" as username$")
+		public void user_enter_as_username(String arg1) throws Throwable {
+		   Input_Element(driver.findElement(By.id("username")), arg1); 
+		}
+		
+		@When("^User enter \"([^\"]*)\" as password$")
+		public void user_enter_as_password(String arg1) throws Throwable {
+		   Input_Element(driver.findElement(By.id("password")), arg1); 
+		}
+		
+		@When("^User enter Mathi as username$")
+		public void user_enter_Mathi_as_username() throws Throwable {
+		  
+		}
+
+		@When("^User enter lalu(\\d+) as password$")
+		public void user_enter_lalu_as_password(int arg1) throws Throwable {
+		   
+		}
+
+		@When("^User enter Bhavan as username$")
+		public void user_enter_Bhavan_as_username() throws Throwable {
+		    
+		}
+
+		@When("^User enter BB@(\\d+) as password$")
+		public void user_enter_BB_as_password(int arg1) throws Throwable {
+		    
+		}
+
+		@When("^User enter Mathu as username$")
+		public void user_enter_Mathu_as_username() throws Throwable {
+		   
+		}
+
+		@When("^User enter njhj(\\d+) as password$")
+		public void user_enter_njhj_as_password(int arg1) throws Throwable {
+		    
+		}
+
+		@When("^User click on the login button$")
+		public void user_click_on_the_login_button() throws Throwable {
+		   Click_Element(driver.findElement(By.id("login"))); 
+		    
+		}
+
+		@Then("^User validate the entred username and password$")
+		public void user_validate_the_entred_username_and_password() throws Throwable {
+		    
+		}
+	}
