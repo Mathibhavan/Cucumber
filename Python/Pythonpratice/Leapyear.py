a013b5b504b4da0311756ffffe3bd57aaddf8d7e
Mathibhavan <61534653+Mathibhavan@users.noreply.github.com>
2021-05-12 18:04:08 +0530

    Delete Python directory

--- Python/Pythonpratice/Leapyear.py
+++
@@ -1,6 +0,0 @@
-from _ast import If
-num=input("Enter the year")
-if (num%4)== 0:
-    print("Leap year")
-else:
-    print("Non Leap year")
--- Python/Pythonpratice/Profitablegame.py
+++
@@ -1,8 +0,0 @@
-from _ast import If
-prob=int(input("Enter the prob"))
-prize=int(input("Enter the prize"))
-pay=int(input("Enter the pay"))
-if prob*prize >pay:
-  print("True")
-else:
-  print("False")
