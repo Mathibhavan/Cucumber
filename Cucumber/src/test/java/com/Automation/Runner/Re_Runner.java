2bf5804bad3b556e4290f5838cbcd4495982fd31
Mathibhavan <61534653+Mathibhavan@users.noreply.github.com>
2021-06-02 20:56:12 +0530

    Runner commit

---
+++ Cucumber/src/test/java/com/Automation/Runner/Re_Runner.java
@@ -0,0 +1,30 @@
+package com.Automation.Runner;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import com.Cucumber.Base_Class.Base_Class;
+import com.github.mkolisnyk.cucumber.runner.ExtendedCucumberOptions;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+
+	@RunWith(Cucumber.class)
+	@ExtendedCucumberOptions(retryCount=4)
+	@CucumberOptions(features="C:\\Users\\721901\\eclipse-workspace\\Cucumber\\src\\test\\java\\com\\Automation\\Feature\\Automation.Feature",
+	glue="com\\Automation\\Stepdefinition", monochrome= true, dryRun=false,strict=true,plugin= {"com.cucumber.listener.ExtentCucumberFormatter:Report//CucumberExtendReport.html"})
+
+	public class Re_Runner {
+
+		public static WebDriver driver;
+		
+	@BeforeClass
+	public static void Setup() {
+		driver=Base_Class.BrowserLaunch("Chrome");
+	}
+	@AfterClass
+	public static void Teardown() {
+		driver.quit();
+	}
+}
